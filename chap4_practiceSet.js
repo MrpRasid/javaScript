9fbe8ae48b2a9c63432ff0375e4791abc4c6ff76
Mamun Rasid Prodhan <mrprasid47@gmail.com>
2023-12-15 20:12:58 +0530

    This is the String chapter
    in this the topic are lowar case to upper case string to int, spacial character ,find word etc..

---
+++ chap4_practiceSet.js
@@ -0,0 +1,19 @@
+
+//Q1 length of the sentence
+let str = "mrp\""
+console.log(str.length)
+
+// Q2
+
+const sentence = "human are not immortal"
+const word = "not"
+// console.log(sentence.includes(word))
+
+console.log(`the word "${word}" ${sentence.includes(word)?'is':'is not '} in the sentence`)
+
+//Q4 string to intiger
+let digit = "1000"
+console.log(typeof digit)
+let amount = Number.parseInt(digit)
+console.log(amount)
+    console.log(typeof amount)
