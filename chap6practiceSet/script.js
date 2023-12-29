d1795c6623b0751762f810aff1041c171287a6c8
Mamun Rasid Prodhan <mrprasid47@gmail.com>
2023-12-29 23:47:18 +0530

    chapter 6 practice Set
    In this chapter we learn Dom and Bom of javaScript aleart,prompt and document

---
+++ chap6practiceSet/script.js
@@ -0,0 +1,47 @@
+//Q1
+//    
+//    a =    prompt("enter your age:")
+//    b = Number.parseInt(a)
+//    if(b>18){
+    // alert("You can drive")
+//    }else{
+//        alert("You can't drive 😊")
+//    }
+
+
+//Q2
+
+//    let runagain =true
+
+//    while(runagain){
+   
+//     a =    prompt("enter your age:")
+//     b = Number.parseInt(a)
+
+//     if(b<0){
+//         console.error("Please enter a valid age")
+//         break
+//     }
+//     if(b>18){
+//         alert("You can drive")
+//     }else{
+//         alert("You can't drive 😊")
+//     }
+//     runagain = confirm("Do you want to run again?")
+//    }
+
+
+//Q3
+
+    // let num = prompt("Enter a number")
+    // let  nums = Number.parseInt(num)
+
+    // if(nums>4){
+    // location.href="https://google.com"
+    // }  
+    
+    
+// Q5
+
+    let color = prompt("Enter a color: ")
+    document.body.style.background =color
